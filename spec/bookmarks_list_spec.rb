2c2bfa93c4cc868923344d9cb9a0a888241a1d68
Meghan <meghan.e.mccue@gmail.com>
2020-01-14 18:12:31 +0000

    Creating Bookmark-draft2.0

--- spec/bookmarks_list_spec.rb
+++ spec/bookmarks_list_spec.rb
@@ -2,7 +2,7 @@ describe 'BookmarksList' do
   describe '.all' do
     it 'returns array of bookmarks' do
       connection = PG.connect(dbname: 'bookmark_manager_test')
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      BookmarkList.create('http://www.makersacademy.com')
       bookmarks = BookmarkList.all
       expect(bookmarks).to include("http://www.makersacademy.com")
     end
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -3,9 +3,7 @@ feature 'View Bookmarks' do
     visit '/'
     click_link 'View Bookmarks'
     connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+    BookmarkList.create('http://www.makersacademy.com')
     expect(page).to have_content "http://www.makersacademy.com"
-    #expect(page).to have_content "http://destroyallsoftware.com"
-    #expect(page).to have_content "http://www.google.com"
   end
 end
